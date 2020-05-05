6f9c50c7d8f07dc97771feb546c594a3237ae0a1
AbigailMcGovern <abigail_mcgovern@hotmail.com>
2020-05-05 19:48:48 +1000

    added an installation bash script and YAML environment file with dependencies/useful packages

---
+++ msbrainpy-install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# if errors emerge (usually relating to allensdk) execute the following:
+# xcode-select --install # MacOS Xcode developer tools
+conda env create -f environment.yml
+conda activate msbrainpy
+pip install allensdk
+sudo python setup.py install
