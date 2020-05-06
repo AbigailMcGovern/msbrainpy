2d520f94a9444d325acac217e589d7f1ac3a50bb
AbigailMcGovern <abigail_mcgovern@hotmail.com>
2020-05-06 11:23:36 +1000

    now with optionally bigger environment

--- msbrainpy-install.sh
+++ msbrainpy-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # if errors emerge (usually relating to allensdk) execute the following:
 # xcode-select --install # MacOS Xcode developer tools
-conda env create -f environment.yml
+conda env create -f environment.yml # or change to environment-big.yml
 conda activate msbrainpy
 pip install allensdk
 pip install requests
