090569e527d8378d90286d068c943f09c3f3f577
AbigailMcGovern <abigail_mcgovern@hotmail.com>
2020-07-16 15:14:40 +1000

    added insitu subpakage and moved code - currently broken

---
+++ msbrainpy/insitu/_gene_series.py
@@ -0,0 +1,7 @@
+
+# File contains functions specific to processing images in a series in situ
+# brain series to resolve information about the brains in the series and the 
+# gene in question.
+# -----------------------------------------------------------------------------
+# Signal quantification
+# ---------------------
