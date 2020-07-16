6bd004f7645da084f7f2999868a715a5a3da0cb6
AbigailMcGovern <abigail_mcgovern@hotmail.com>
2020-07-16 10:28:11 +1000

    remove sudo

--- msbrainpy-install.sh
+++ msbrainpy-install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 # if errors emerge (usually relating to allensdk) execute the following:
 # xcode-select --install # MacOS Xcode developer tools
+# or installing homebrew should automatically install xcode conmmand line dev tools
 conda env create -f environment.yml # or change to environment-big.yml
 conda activate msbrainpy
 pip install allensdk
 pip install requests
-sudo python setup.py install
+python setup.py install
