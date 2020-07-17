3670988c17bdfea07952c276e346deafe9d630a6
AbigailMcGovern <abigail_mcgovern@hotmail.com>
2020-07-17 23:07:53 +1000

    changed insitu to brainseries, WIP, non-functional

--- msbrainpy/insitu/_gene_series.py
+++
@@ -1,7 +0,0 @@
-
-# File contains functions specific to processing images in a series in situ
-# brain series to resolve information about the brains in the series and the 
-# gene in question.
-# -----------------------------------------------------------------------------
-# Signal quantification
-# ---------------------
